9d98d1756dc1325daf53c53aa97a37ca53d50c2c
NOCTLJRNE <baoan1602@gmail.com>
2020-05-12 17:11:51 +0200

    add nuxt proxy on port 8080

--- nuxtProxy.js
+++ nuxtProxy.js
@@ -3,7 +3,7 @@ var app = express();
 var httpProxy = require("http-proxy");
 var apiProxy = httpProxy.createProxyServer();
 var serverOne = "http://localhost:3000";
-const PORT = 80;
+const PORT = process.env.PORT;
 app.all("/*", function (req, res) {
   apiProxy.web(req, res, { target: serverOne });
 });
